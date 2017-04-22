045a965d49f02fe7f2007c62fd0f05f51ba15a77
Xuebing Li <xuebinglee@gmail.com>
2017-04-22 15:01:21 +1000

    run all tests with diagonal sudoku

--- solution_spec.py
+++ solution_spec.py
@@ -2,65 +2,63 @@ import solution
 from solution import DIGITS
 import unittest
 
-class TestStandardSudoku(unittest.TestCase):
-    grid = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
+class TestDiagonalSudoku(unittest.TestCase):
+    grid = '.5.......6.3..24...7.1....38.4.....7.........3.....2.97....1.2...96..7.1.......4.'
     values = {
-        'A1': DIGITS, 'A2': DIGITS, 'A3': '3', 'A4': DIGITS, 'A5': '2', 'A6': DIGITS, 'A7': '6', 'A8': DIGITS, 'A9': DIGITS,
-        'B1': '9', 'B2': DIGITS, 'B3': DIGITS, 'B4': '3', 'B5': DIGITS, 'B6': '5', 'B7': DIGITS, 'B8': DIGITS, 'B9': '1',
-        'C1': DIGITS, 'C2': DIGITS, 'C3': '1', 'C4': '8', 'C5': DIGITS, 'C6': '6', 'C7': '4', 'C8': DIGITS, 'C9': DIGITS,
-        'D1': DIGITS, 'D2': DIGITS, 'D3': '8', 'D4': '1', 'D5': DIGITS, 'D6': '2', 'D7': '9', 'D8': DIGITS, 'D9': DIGITS,
-        'E1': '7', 'E2': DIGITS, 'E3': DIGITS, 'E4': DIGITS, 'E5': DIGITS, 'E6': DIGITS, 'E7': DIGITS, 'E8': DIGITS, 'E9': '8',
-        'F1': DIGITS, 'F2': DIGITS, 'F3': '6', 'F4': '7', 'F5': DIGITS, 'F6': '8', 'F7': '2', 'F8': DIGITS, 'F9': DIGITS,
-        'G1': DIGITS, 'G2': DIGITS, 'G3': '2', 'G4': '6', 'G5': DIGITS, 'G6': '9', 'G7': '5', 'G8': DIGITS, 'G9': DIGITS,
-        'H1': '8', 'H2': DIGITS, 'H3': DIGITS, 'H4': '2', 'H5': DIGITS, 'H6': '3', 'H7': DIGITS, 'H8': DIGITS, 'H9': '9',
-        'I1': DIGITS, 'I2': DIGITS, 'I3': '5', 'I4': DIGITS, 'I5': '1', 'I6': DIGITS, 'I7': '3', 'I8': DIGITS, 'I9': DIGITS
+        'A1': DIGITS, 'A2': '5',    'A3': DIGITS, 'A4': DIGITS, 'A5': DIGITS, 'A6': DIGITS, 'A7': DIGITS, 'A8': DIGITS, 'A9': DIGITS,
+        'B1': '6',    'B2': DIGITS, 'B3': '3',    'B4': DIGITS, 'B5': DIGITS, 'B6': '2',    'B7': '4',    'B8': DIGITS, 'B9': DIGITS,
+        'C1': DIGITS, 'C2': '7',    'C3': DIGITS, 'C4': '1',    'C5': DIGITS, 'C6': DIGITS, 'C7': DIGITS, 'C8': DIGITS, 'C9': '3',
+        'D1': '8',    'D2': DIGITS, 'D3': '4',    'D4': DIGITS, 'D5': DIGITS, 'D6': DIGITS, 'D7': DIGITS, 'D8': DIGITS, 'D9': '7',
+        'E1': DIGITS, 'E2': DIGITS, 'E3': DIGITS, 'E4': DIGITS, 'E5': DIGITS, 'E6': DIGITS, 'E7': DIGITS, 'E8': DIGITS, 'E9': DIGITS,
+        'F1': '3',    'F2': DIGITS, 'F3': DIGITS, 'F4': DIGITS, 'F5': DIGITS, 'F6': DIGITS, 'F7': '2',    'F8': DIGITS, 'F9': '9',
+        'G1': '7',    'G2': DIGITS, 'G3': DIGITS, 'G4': DIGITS, 'G5': DIGITS, 'G6': '1',    'G7': DIGITS, 'G8': '2',    'G9': DIGITS,
+        'H1': DIGITS, 'H2': DIGITS, 'H3': '9',    'H4': '6',    'H5': DIGITS, 'H6': DIGITS, 'H7': '7',    'H8': DIGITS, 'H9': '1',
+        'I1': DIGITS, 'I2': DIGITS, 'I3': DIGITS, 'I4': DIGITS, 'I5': DIGITS, 'I6': DIGITS, 'I7': DIGITS, 'I8': '4',    'I9': DIGITS
     }
+
+    def test_grid_values(self):
+        self.assertEqual(solution.grid_values(self.grid), self.values)
 
     def test_cross(self):
         rows = 'ABC'
         digits = '12'
         self.assertEqual(solution.cross(rows, digits), ['A1', 'A2', 'B1', 'B2', 'C1', 'C2'])
 
-    def test_grid_values(self):
-        self.assertEqual(solution.grid_values(self.grid), self.values)
-
     def test_in_three(self):
         self.assertEqual(solution.in_three('ABCDEFGHI'), ['ABC', 'DEF', 'GHI'])
 
     def test_territory_of_square(self):
         self.assertEqual(solution.territory_of_square('E2'), ['D1', 'D2', 'D3',
-                                                         'E1', 'E2', 'E3',
-                                                         'F1', 'F2', 'F3'])
+                                                              'E1', 'E2', 'E3',
+                                                              'F1', 'F2', 'F3'])
         self.assertEqual(solution.territory_of_square('I9'), ['G7', 'G8', 'G9',
-                                                         'H7', 'H8', 'H9',
-                                                         'I7', 'I8', 'I9'])
+                                                              'H7', 'H8', 'H9',
+                                                              'I7', 'I8', 'I9'])
 
-    def test_only_choice(self):
-        eliminated_values = self.values.copy()
-        eliminated_values['A4'] = '49'
-        eliminated_values['A6'] = '147'
-        eliminated_values['B5'] = '47'
-        eliminated_values['C5'] = '79'
-        processed_values = solution.only_choice(eliminated_values)
-        self.assertEqual(processed_values['A6'], '1')
-
-class TestDiagonalSudoku(unittest.TestCase):
-    values = {
-        'A1': DIGITS, 'A2': '5', 'A3': DIGITS, 'A4': DIGITS, 'A5': DIGITS, 'A6': DIGITS, 'A7': DIGITS, 'A8': DIGITS, 'A9': DIGITS,
-        'B1': '6', 'B2': DIGITS, 'B3': '3', 'B4': DIGITS, 'B5': DIGITS, 'B6': '2', 'B7': '4', 'B8': DIGITS, 'B9': DIGITS,
-        'C1': DIGITS, 'C2': '7', 'C3': DIGITS, 'C4': '1', 'C5': DIGITS, 'C6': DIGITS, 'C7': DIGITS, 'C8': DIGITS, 'C9': '3',
-        'D1': '8', 'D2': DIGITS, 'D3': '4', 'D4': DIGITS, 'D5': DIGITS, 'D6': DIGITS, 'D7': DIGITS, 'D8': DIGITS, 'D9': '7',
-        'E1': DIGITS, 'E2': DIGITS, 'E3': DIGITS, 'E4': DIGITS, 'E5': DIGITS, 'E6': DIGITS, 'E7': DIGITS, 'E8': DIGITS, 'E9': DIGITS,
-        'F1': '3', 'F2': DIGITS, 'F3': DIGITS, 'F4': DIGITS, 'F5': DIGITS, 'F6': DIGITS, 'F7': '2', 'F8': DIGITS, 'F9': '9',
-        'G1': '7', 'G2': DIGITS, 'G3': DIGITS, 'G4': DIGITS, 'G5': DIGITS, 'G6': '1', 'G7': DIGITS, 'G8': '2', 'G9': DIGITS,
-        'H1': DIGITS, 'H2': DIGITS, 'H3': '9', 'H4': '6', 'H5': DIGITS, 'H6': DIGITS, 'H7': '7', 'H8': DIGITS, 'H9': '1',
-        'I1': DIGITS, 'I2': DIGITS, 'I3': DIGITS, 'I4': DIGITS, 'I5': DIGITS, 'I6': DIGITS, 'I7': DIGITS, 'I8': '4', 'I9': DIGITS
-    }
+    def test_is_unsolvable(self):
+        unsolvable_values = self.values.copy()
+        unsolvable_values['A1'] = ''
+        self.assertTrue(solution.is_unsolvable(unsolvable_values))
 
     def test_eliminate(self):
-        eliminated_values = solution.eliminate(self.values)
+        eliminated_values = solution.eliminate(self.values.copy())
         self.assertEqual(eliminated_values['E2'], '1269')
         self.assertEqual(eliminated_values['A1'], '1249')
+
+    def test_only_choice(self):
+        eliminated_values = solution.eliminate(self.values.copy())
+        processed_values = solution.only_choice(eliminated_values)
+        self.assertEqual(processed_values, {
+            'A1': '1249', 'A2': '5', 'A3': '128', 'A4': '34789', 'A5': '346789', 'A6': '346789', 'A7': '1689', 'A8': '16789', 'A9': '2',
+            'B1': '6', 'B2': '189', 'B3': '3', 'B4': '5789', 'B5': '5789', 'B6': '2', 'B7': '4', 'B8': '15789', 'B9': '58',
+            'C1': '249', 'C2': '7', 'C3': '28', 'C4': '1', 'C5': '45689', 'C6': '45689', 'C7': '5689', 'C8': '5689', 'C9': '3',
+            'D1': '8', 'D2': '1269', 'D3': '4', 'D4': '2359', 'D5': '123569', 'D6': '3569', 'D7': '1356', 'D8': '1356', 'D9': '7',
+            'E1': '1259', 'E2': '1269', 'E3': '12567', 'E4': '2345789', 'E5': '123456789', 'E6': '3456789', 'E7': '13568', 'E8': '13568', 'E9': '4',
+            'F1': '3', 'F2': '16', 'F3': '1567', 'F4': '4578', 'F5': '145678', 'F6': '45678', 'F7': '2', 'F8': '1568', 'F9': '9',
+            'G1': '7', 'G2': '3468', 'G3': '568', 'G4': '34589', 'G5': '34589', 'G6': '1', 'G7': '35689', 'G8': '2', 'G9': '568',
+            'H1': '245', 'H2': '2348', 'H3': '9', 'H4': '6', 'H5': '23458', 'H6': '3458', 'H7': '7', 'H8': '358', 'H9': '1',
+            'I1': '125', 'I2': '12368', 'I3': '12568', 'I4': '235789', 'I5': '235789', 'I6': '35789', 'I7': '35689', 'I8': '4', 'I9': '568'
+        })
 
     def test_peers(self):
         self.assertCountEqual(solution.peers('E2'), set(['E1', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8', 'E9',
